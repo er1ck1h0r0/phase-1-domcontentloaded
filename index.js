2aa7ca7fe92d2ed3c92e6d443dc65643f16e9bbc
eric <ericmaina11455@gmail.com>
2024-04-07 11:15:22 +0300

    event-listener

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+
+document.addEventListener("DOMContentLoaded",(e) => {
+    const change =document.getElementById("text");
+    change.textContent ="This is really cool!";
+})
